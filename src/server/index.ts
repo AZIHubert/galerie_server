7b76d49c4289f32f0370e0f0f3098f2f8f2a2b26
AZIHubert <allan.aoudji@protonmail.com>
2021-01-24 21:52:33 +0100

    remove headers middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -58,11 +58,6 @@ const initApp: () => http.Server = () => {
       origin: '*',
     }),
   );
-  app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
   app.use('/users', userRouter(io));
   io.on('connection', (socket) => {
     socket.on('disconnect', () => {});
