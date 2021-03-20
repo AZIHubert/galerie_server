e47bdbf94db7ea0d10ddee50fc6b4c4737b7f780
AZIHubert <allan.aoudji@protonmail.com>
2021-03-20 21:50:24 +0100

    route GET /galeries/:id/users

--- src/routes/galerie/index.ts
+++ src/routes/galerie/index.ts
@@ -3,21 +3,21 @@ import { Router } from 'express';
 import {
   uploadFiles,
 } from '@src/helpers/middlewares';
-import signedUrl from '@src/helpers/signedUrl';
 import passport from '@src/helpers/passport';
 
 import {
-  User,
   Galerie,
-  ProfilePicture,
-  Image,
+  GalerieUser,
+  User,
 } from '@src/db/models';
+
 import {
   deleteGaleriesIdFramesId,
   getGaleries,
   getGaleriesId,
   getGaleriesIdFrames,
   getGaleriesIdFramesId,
+  getGaleriesIdUsers,
   postGaleries,
   postGaleriesIdFrames,
   putGaleriesId,
@@ -52,146 +52,7 @@ const galeriesRoutes: () => Router = () => {
     // and belong to this galerie
     // return this invit
   }); // get a invit to a galerie
-  router.get('/:id/users', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    const { id: userId } = req.user as User;
-    const { id: galerieId } = req.params;
-    let galerie: Galerie | null;
-    let users: User[];
-    try {
-      galerie = await Galerie.findByPk(galerieId, {
-        include: [{
-          model: User,
-          where: {
-            id: userId,
-          },
-        }],
-      });
-    } catch (err) {
-      return res.status(500).send(err);
-    }
-    if (!galerie) {
-      return res.status(404).send({
-        errors: 'galerie not found',
-      });
-    }
-    try {
-      users = await User.findAll({
-        attributes: {
-          exclude: [
-            'authTokenVersion',
-            'blackListId',
-            'confirmed',
-            'confirmTokenVersion',
-            'currentProfilePictureId',
-            'email',
-            'emailTokenVersion',
-            'googleId',
-            'password',
-            'resetPasswordTokenVersion',
-            'updatedEmailTokenVersion',
-          ],
-        },
-        include: [{
-          model: Galerie,
-          where: {
-            id: galerieId,
-          },
-        }, {
-          model: ProfilePicture,
-          as: 'currentProfilePicture',
-          attributes: {
-            exclude: [
-              'createdAt',
-              'cropedImageId',
-              'deletedAt',
-              'originalImageId',
-              'pendingImageId',
-              'updatedAt',
-              'userId',
-            ],
-          },
-          include: [
-            {
-              model: Image,
-              as: 'cropedImage',
-              attributes: {
-                exclude: [
-                  'createdAt',
-                  'deletedAt',
-                  'updatedAt',
-                ],
-              },
-            },
-            {
-              model: Image,
-              as: 'originalImage',
-              attributes: {
-                exclude: [
-                  'createdAt',
-                  'deletedAt',
-                  'updatedAt',
-                ],
-              },
-            },
-            {
-              model: Image,
-              as: 'pendingImage',
-              attributes: {
-                exclude: [
-                  'createdAt',
-                  'deletedAt',
-                  'updatedAt',
-                ],
-              },
-            },
-          ],
-        }],
-      });
-      await Promise.all(
-        users.map(async (user, index) => {
-          if (user.currentProfilePicture) {
-            const {
-              currentProfilePicture: {
-                cropedImage: {
-                  bucketName: cropedImageBucketName,
-                  fileName: cropedImageFileName,
-                },
-                originalImage: {
-                  bucketName: originalImageBucketName,
-                  fileName: originalImageFileName,
-                },
-                pendingImage: {
-                  bucketName: pendingImageBucketName,
-                  fileName: pendingImageFileName,
-                },
-              },
-            } = user;
-            const cropedImageSignedUrl = await signedUrl(
-              cropedImageBucketName,
-              cropedImageFileName,
-            );
-            users[index].currentProfilePicture.cropedImage.signedUrl = cropedImageSignedUrl;
-            const originalImageSignedUrl = await signedUrl(
-              originalImageBucketName,
-              originalImageFileName,
-            );
-            users[index].currentProfilePicture.originalImage.signedUrl = originalImageSignedUrl;
-            const pendingImageSignedUrl = await signedUrl(
-              pendingImageBucketName,
-              pendingImageFileName,
-            );
-            users[index].currentProfilePicture.pendingImage.signedUrl = pendingImageSignedUrl;
-          }
-        }),
-      );
-    } catch (err) {
-      return res.status(500).send(err);
-    }
-    return res.status(200).send(users);
-    // check if galerie exist
-    // check if user have subscribe to this galerie
-    // return all users to this galerie (limit 20)
-  }); // get all user from a galerie
+  router.get('/:id/users', passport.authenticate('jwt', { session: false }), getGaleriesIdUsers); // DONE
   router.post('/', passport.authenticate('jwt', { session: false }), postGaleries); // DONE
   router.post('/:id/frames', passport.authenticate('jwt', { session: false }), uploadFiles, postGaleriesIdFrames); // DONE
   router.put('/:id', passport.authenticate('jwt', { session: false }), putGaleriesId); // DONE
@@ -215,7 +76,89 @@ const galeriesRoutes: () => Router = () => {
     // remove galerieUser to this galerie
     // remove all frame upload by this user
   }); // unsubscribe to a galerie
-  router.put('/:id/users/:userId', () => {
+  router.delete('/:id/users/:userId', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    const { id: userId } = req.user as User;
+    const { id: galerieId, userId: UId } = req.params;
+    let galerie: Galerie | null;
+    if (userId === UId) {
+      return res.status(400).send({
+        errors: 'you cannot delete yourself',
+      });
+    }
+    try {
+      galerie = await Galerie.findByPk(galerieId, {
+        include: [{
+          model: User,
+          where: {
+            id: userId,
+          },
+        }],
+      });
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+    if (!galerie) {
+      return res.status(404).send({
+        errors: 'galerie not found',
+      });
+    }
+    const { role } = galerie
+      .users
+      .filter((user) => user.id === userId)[0]
+      .GalerieUser;
+    let user: User | null;
+    if (role === 'user') {
+      return res.status(400).send({
+        errors: 'you should be an admin or the creator to this galerie to delete a user',
+      });
+    }
+    try {
+      user = await User.findOne({
+        where: {
+          id: UId,
+        },
+        include: [{
+          model: Galerie,
+          where: {
+            id: galerieId,
+          },
+        }],
+      });
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+    if (!user) {
+      return res.status(404).send({
+        errors: 'user not found',
+      });
+    }
+    if (user.GalerieUser.role === 'creator') {
+      return res.status(400).send({
+        errors: 'you can\'t delete the creator of this galerie',
+      });
+    }
+    if (
+      user.GalerieUser.role === 'admin'
+      && role !== 'creator'
+    ) {
+      return res.status(400).send({
+        errors: 'you should be the creator of this galerie to delete an admin',
+      });
+    }
+    try {
+      await GalerieUser.destroy({
+        where: {
+          userId: UId,
+          galerieId,
+        },
+      });
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+    return res.status(200).end({
+      userId: UId,
+      galerieId,
+    });
     // check if galerie exist
     // check if user is creator or admin
     // check if user userId exist
--- src/routes/galerie/routes/index.ts
+++ src/routes/galerie/routes/index.ts
@@ -3,6 +3,7 @@ export { default as getGaleries } from './getGaleries';
 export { default as getGaleriesId } from './getGaleriesId';
 export { default as getGaleriesIdFrames } from './getGaleriesIdFrames';
 export { default as getGaleriesIdFramesId } from './getGaleriesIdFramesId';
+export { default as getGaleriesIdUsers } from './getGaleriesIdUsers';
 export { default as postGaleries } from './postGaleries';
 export { default as postGaleriesIdFrames } from './postGaleriesIdFrames';
 export { default as putGaleriesId } from './putGaleriesId';
