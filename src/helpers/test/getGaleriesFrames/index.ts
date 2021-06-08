6e40f36630926666a9ec5d3db1c4460c5095fbdb
AZIHubert <allan.aoudji@protonmail.com>
2021-06-08 21:19:54 +0200

    created helpers/test/getGaleriesFrames

---
+++ src/helpers/test/getGaleriesFrames/index.ts
@@ -0,0 +1,17 @@
+import { Server } from 'http';
+import request from 'supertest';
+
+export default async (
+  app: Server,
+  token: string,
+  option: {
+    page: number;
+  } = {
+    page: 1,
+  },
+) => {
+  const response = await request(app)
+    .get(`/galeries/frames?page=${option.page}`)
+    .set('authorization', token);
+  return response;
+};
