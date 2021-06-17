1d2334ebb0fcbdf29f2e57f67bf3ad8443e956ca
AZIHubert <allan.aoudji@protonmail.com>
2021-06-17 23:23:17 +0200

    create router skeleton

--- src/routes/betaKeys/index.ts
+++ src/routes/betaKeys/index.ts
@@ -1,3 +1,32 @@
 // BetaKey
 //  code
 //  userId (the user who use this beta key to subscribe)
+
+import { Router } from 'express';
+
+import {
+  shouldBeSuperAdmin,
+} from '@src/helpers/middlewares';
+
+const router = Router();
+
+const betaKeyRoutes: () => Router = () => {
+  router.delete('/:betaKeyId', shouldBeSuperAdmin, () => {});
+
+  router.get('/', shouldBeSuperAdmin, () => {});
+  router.get('/:betaKeyId', shouldBeSuperAdmin, () => {});
+  router.get('/me', shouldBeSuperAdmin, () => {});
+  router.get('/used', shouldBeSuperAdmin, () => {});
+
+  router.post('/', shouldBeSuperAdmin, () => {});
+
+  return router;
+};
+
+export default betaKeyRoutes;
+
+// TODO:
+// BetaKey model
+//  adminId
+//  userId
+//  usedAt
