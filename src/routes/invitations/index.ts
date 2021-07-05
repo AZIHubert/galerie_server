adac6c6b88ec949e2b19634e6230ee22d7bd3b24
AZIHubert <allan.aoudji@protonmail.com>
2021-07-05 16:55:38 +0200

    remove GET /invitations/qrCode/

--- src/routes/invitations/index.ts
+++ src/routes/invitations/index.ts
@@ -17,16 +17,6 @@ const invitationsRoute: () => Router = () => {
 
   router.get('/:invitationId/', shouldBeAuth, getInvitationsId);
 
-  // use invitationToken
-  // when get /invitation/:invitationId
-  // return a unique jwt used to generate the QRcode.
-  // for now, not sure if qrcode is supposed to be generated
-  // by the server or the client
-  // but in case it's suppose to be the server
-  // this route gonna be used to generate the QRcode.
-  // public
-  router.get('/qrCode/', shouldBeAuth, () => {});
-
   return router;
 };
 
