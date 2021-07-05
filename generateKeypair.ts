2a6907425cc4cace259ee2e43eb3b4729d122106
AZIHubert <allan.aoudji@protonmail.com>
2021-07-05 16:53:24 +0200

    remove invitation generate keypair

--- generateKeypair.ts
+++ generateKeypair.ts
@@ -14,18 +14,6 @@ const genKeyPair = () => {
     },
   });
 
-  const keyPairInvitationToken = crypto.generateKeyPairSync('rsa', {
-    modulusLength: 4096,
-    publicKeyEncoding: {
-      type: 'pkcs1',
-      format: 'pem',
-    },
-    privateKeyEncoding: {
-      type: 'pkcs1',
-      format: 'pem',
-    },
-  });
-
   const keyPairNotificationToken = crypto.generateKeyPairSync('rsa', {
     modulusLength: 4096,
     publicKeyEncoding: {
@@ -52,9 +40,6 @@ const genKeyPair = () => {
 
   fs.writeFileSync(`${__dirname}/id_rsa_pub.authToken.pem`, keyPairAuthToken.publicKey);
   fs.writeFileSync(`${__dirname}/id_rsa_priv.authToken.pem`, keyPairAuthToken.privateKey);
-
-  fs.writeFileSync(`${__dirname}/id_rsa_pub.invitationToken.pem`, keyPairInvitationToken.publicKey);
-  fs.writeFileSync(`${__dirname}/id_rsa_priv.invitationToken.pem`, keyPairInvitationToken.privateKey);
 
   fs.writeFileSync(`${__dirname}/id_rsa_pub.notificationToken.pem`, keyPairNotificationToken.publicKey);
   fs.writeFileSync(`${__dirname}/id_rsa_priv.notificationToken.pem`, keyPairNotificationToken.privateKey);
